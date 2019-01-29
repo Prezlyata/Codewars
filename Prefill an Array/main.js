4d3258495f54fce8b233042a563e59cb2f5fd950
Taras <taras.prezlyata.sign.in@gmail.com>
2019-01-30 00:38:17 +0200

    Prefill an Array

---
+++ Prefill an Array/main.js
@@ -0,0 +1,14 @@
+function prefill(n, v) {
+	let arrRez = [];
+	let check = NaN;
+	let error = new Error();
+	for(let i= 0; i<n; i++){
+		arrRez.push(v)
+	}
+	if(arrRez.length == 0){
+		throw new TypeError(n+" is invalid");
+	}else{
+		return arrRez
+	}
+}
+console.log(prefill("ds", 1))
